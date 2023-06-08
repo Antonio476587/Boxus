ce112ae47679a3ea849783dd7fa146ce001c501f
fantonix <fantonix.web@gmail.com>
2023-06-08 14:10:01 -0400

    feat(env): Adding @ngx-env/builder and importing the env variables in environment

--- frontend/src/app/services/my-secrets.service.ts
+++ frontend/src/app/services/my-secrets.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, take, tap } from 'rxjs';
 import { FullSecret, Secret } from '../interfaces/secret';
-import { environment } from '../../environments/environment';
+import { environment } from '../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class MySecretsService {
